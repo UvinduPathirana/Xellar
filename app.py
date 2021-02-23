da9dd1f8d2089a78d7c978b1b2ab0377697c9187
Uvindu-bit <upathirana112@gmail.com>
2021-02-23 16:10:23 +0530

    app.py update

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ app = Flask(__name__,  static_url_path='',
                     static_folder='static', template_folder='templates')
                     
 # local
-application.config['MYSQL_HOST'] = 'localhost'
-application.config['MYSQL_USER'] = 'root'
-application.config['MYSQL_PASSWORD'] = 'Uv1ndu2006'
-application.config['MYSQL_DB'] = 'home_delivary'
+# application.config['MYSQL_HOST'] = 'localhost'
+# application.config['MYSQL_USER'] = 'root'
+# application.config['MYSQL_PASSWORD'] = 'Uv1ndu2006'
+# application.config['MYSQL_DB'] = 'home_delivary'
 
 # production
 # application.config['MYSQL_HOST'] = 'database-1.c5qdsuoy5mft.ap-south-1.rds.amazonaws.com'
@@ -19,4 +19,6 @@ application.config['MYSQL_DB'] = 'home_delivary'
 # application.config['MYSQL_PASSWORD'] = ''
 # application.config['MYSQL_DB'] = 'home_delivary'
 
-mysql = MySQL(application)
+@app.route('/')
+def hello_world():
+    return  ('Hello')
