b20b6b283f5e80af1dae27bb153df06484146faf
Uvindu-bit <upathirana112@gmail.com>
2021-02-23 22:15:27 +0530

    app update2

--- app.py
+++ app.py
@@ -23,14 +23,14 @@ app = Flask(__name__,  static_url_path='',
 def hello_world():
     return render_template('index.html')
 
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
+@app.route('/main')
+def main():
+    return render_template('main.html')
 
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
+@app.route('/shopping-cart')
+def cart():
+    return render_template('shopping-cart.html')
