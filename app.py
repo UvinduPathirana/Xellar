4b7aef9033e17c741b1e54ba8f50a6b0d9077a9a
Uvindu-bit <upathirana112@gmail.com>
2021-02-23 15:56:00 +0530

    new commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, flash, redirect, render_template, request, session, abort
+from flask import render_template
+from flask_mysqldb import MySQL
+from markupsafe import escape
+
+
+app = Flask(__name__,  static_url_path='',
+                    static_folder='static', template_folder='templates')
+                    
+# local
+application.config['MYSQL_HOST'] = 'localhost'
+application.config['MYSQL_USER'] = 'root'
+application.config['MYSQL_PASSWORD'] = 'Uv1ndu2006'
+application.config['MYSQL_DB'] = 'home_delivary'
+
+# production
+# application.config['MYSQL_HOST'] = 'database-1.c5qdsuoy5mft.ap-south-1.rds.amazonaws.com'
+# application.config['MYSQL_USER'] = 'admin'
+# application.config['MYSQL_PASSWORD'] = ''
+# application.config['MYSQL_DB'] = 'home_delivary'
+
+mysql = MySQL(application)
