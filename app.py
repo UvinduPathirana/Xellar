ce1b80b778e885f0cf18cb1a9d83dc1ba8514fc5
Uvindu-bit <upathirana112@gmail.com>
2021-03-04 13:12:43 +0530

    category update

--- app.py
+++ app.py
@@ -34,9 +34,11 @@ def hello_world():
     cur.execute("SELECT * FROM cars WHERE cars.range = 2;")
     midrange = cur.fetchall()
     cur.execute("SELECT * FROM cars WHERE cars.range = 3;")
+    category = cur.fetchall()
+    cur.execute("SELECT * FROM cars WHERE cars.range = 4;")
     highend = cur.fetchall()
     cur.close()
-    return render_template('index.html', categories=categories, cars=cars, lowbudget=lowbudget, midrange=midrange, highend=highend)
+    return render_template('index.html', categories=categories, cars=cars, lowbudget=lowbudget, midrange=midrange, highend=highend, category=category)
 
 @app.route('/login')
 def login():
